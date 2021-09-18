0d6214f51c6b74ebdc14ecd202a08043b5891cff
Manuel Bolla Agrelo <manubolla17@gmail.com>
2021-09-18 20:45:42 -0300

    fix filter bug

--- screens/QRListScreen.tsx
+++ screens/QRListScreen.tsx
@@ -9,7 +9,6 @@ import { Application, QRType } from '../types/Application';
 const QRListScreen = () => {
 	const QRList = useSelector<Application, QRType[]>((state) => state.QRList);
 
-	const textInputRef = useRef<TextInput>(null);
 	const dispatch = useDispatch();
 	const [deleteMode, setDeleteMode] = useState(false);
 	const [data, setData] = useState(QRList);
@@ -18,6 +17,7 @@ const QRListScreen = () => {
 		dispatch(deleteQRData());
 		setDeleteMode(false);
 	};
+
 	const handleChange = (value: string) => {
 		if (data) {
 			if (!value) {
@@ -28,9 +28,6 @@ const QRListScreen = () => {
 				item.value.toLowerCase().includes(value.toLocaleLowerCase())
 			);
 			setData(filteredList);
-			if (filteredList.length == 0) {
-				if (textInputRef && textInputRef.current) textInputRef.current.blur();
-			}
 		}
 		return;
 	};
@@ -40,14 +37,13 @@ const QRListScreen = () => {
 	return (
 		<SafeAreaView style={styles.container}>
 			<TextInput
-				ref={textInputRef}
-				placeholder={data.length == 0 ? 'No list to filter' : 'Filter list'}
+				placeholder={QRList.length == 0 ? 'No list to filter' : 'Filter list'}
 				onChangeText={handleChange}
 				style={[
 					styles.input,
-					data.length == 0 && { backgroundColor: '#F3F1F5', borderColor: '#C9CCD5' },
+					QRList.length == 0 && { backgroundColor: '#F3F1F5', borderColor: '#C9CCD5' },
 				]}
-				editable={data.length !== 0}
+				editable={QRList.length !== 0}
 			/>
 			<View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
 				<Button title={'Delete items'} disabled={!deleteMode} onPress={handleDelete} />
